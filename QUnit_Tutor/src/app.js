2a1feadf4bad37418515aadc07f5a355ed0d18df
clif edwards <clif.edwards.2@gmail.com>
2015-06-08 06:08:15 -0400

    ver 2.1.2 ADDED  more of my learning files to git.

---
+++ QUnit_Tutor/src/app.js
@@ -0,0 +1,46 @@
+/**
+ * Created by CLIF on 4/14/2015.
+ */
+
+
+function MyLog(text) {
+    /**
+     * used to see log immediately, rather than on next operation.
+     */
+    console.log(JSON.stringify(text));
+}
+
+function countThisWord (targetArr) {
+    /**
+     *
+     * @type {*|jQuery}
+     */
+    // make Array of Words in this Chapter of Text.
+
+    var text = $('div').text();
+
+    result = text.replace(/(bapt[^\s,-]+)/ig, "<span id='bap'>$&</span>");
+}
+
+
+function click_a_verse () {
+    $('p').click(function () {
+        $(this).toggle(1000, function () {
+            //$(this).load('test_page_1.html');
+            //alert(" toggled() a verse. BUT seeable after this alert.");
+            $(this).addClass('expand');
+            $(this).toggle(1000);
+        })
+    });
+    $('button').click( function() {
+        $('#1').load('test_page_1.html');
+    })
+}
+
+var main = function () {
+    click_a_verse();
+    //findText();
+    //countThisWord(['bapti', 'brother', "Jacob", "Holy Ghost"]);
+};
+
+//$(document).ready(main);
---
+++ QUnit_Tutor/tst/tests.js
@@ -0,0 +1,12 @@
+/**
+ * Created by CLIF on 6/5/2015.
+ */
+
+module( "Learn how to test");
+
+QUnit.test( "a basic test example", function( assert ) {
+    var value = "hello";
+    assert.equal( value, "hello", "We expect value to be hello" );
+
+
+});
