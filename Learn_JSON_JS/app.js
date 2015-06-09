712b6cc3a6eaf30273966908d052d79a8680d655
clif edwards <clif.edwards.2@gmail.com>
2015-06-09 11:56:27 -0400

    ver 2.2.3 2   Can HANDLE  JSON data generated by python in html / JS    BUT don't know how to get it from a local file into JS. IN Learn_JSON_JS IN BRANCH:  LEARN_JSON

--- Learn_JSON_JS/app.js
+++ Learn_JSON_JS/app.js
@@ -1,48 +1,21 @@
 $(document).ready(function() {
-    var b = "hey, I hope to see this.";
+    var jsonfilepath = "C:\\Users\\CLIF\\My Projects\\.PyCharm30\\SeeScriptures\\dat\\sltl.json";
 
-    $("#id01").html(b)
-    console.log("just SET #id01");
+    var somedat = "[u'1', u'And', u'now', u'I', u',', u'Nephi', u',', u'make', u'an', u'end', u'of', u'my', u'prophesying', u'unto', u'you', u',', u'my', u'beloved', u'brethren', u'.']";
+
+    console.log("just successfully logged somedat >> " + somedat);
+
+    $("#id01").html(somedat)
+    console.log("just successfully put somedat into #id01.");
+
+
 
     var aFunction = function (x) {
         console.log("IN aFunction()");
         var a = "I'm in aFunction.";
-        $("#id02").html(x + ", " + a);
+        $("#id02").html(a + ", " + x);
     };
 
-    aFunction(b);
-
-
-    //function anArray() {
-    //    return [
-    //        {
-    //            "display": "JavaScript Tutorial",
-    //            "url": "http://www.w3schools.com/js/default.asp"
-    //        },
-    //        {
-    //            "display": "jQuery Tutorial",
-    //            "url": "http://www.w3schools.com/jquery/default.asp"
-    //        },
-    //        {
-    //            "display": "HTML Tutorial",
-    //            "url": "http://www.w3schools.com/html/default.asp"
-    //        },
-    //        {
-    //            "display": "CSS Tutorial",
-    //            "url": "http://www.w3schools.com/css/default.asp"
-    //        }
-    //    ];
-    //
-    //}
-
-    //function myFunction(myArry) {
-    //    var out = "hi there ";
-    //    var i;
-    //    for (i = 0; i < myArry.length; i++) {
-    //        out += '<a href="' + myArry[i].url + '">' +
-    //            myArry[i].display + '</a><br>';
-    //    }
-    //    document.getElementById("id01").innerHTML = out;
-    //}
+    aFunction(jsonfilepath);
 
 });
