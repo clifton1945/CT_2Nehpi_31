13b0a4c2ace35069e91f4300aafba739d4c34144
clif edwards <clif.edwards.2@gmail.com>
2015-06-13 16:43:52 -0400

    ver 2.3.2   fixing globDat()
    IN Learn_JSON_JS
    IN BRANCH:  LEARN_JSON

--- Learn_JSON_JS/app.js
+++ Learn_JSON_JS/app.js
@@ -2,46 +2,37 @@ $(document).ready(function() {
     // this un var-ed declaration makes it global.
     //noinspection JSUndeclaredVariable
     globals = {
-        "all": "",
-        "sliced": "",
+        "sentLst": "",
+        "sent01": "",
         "something_else": ""
     };
 
     // pull actual data from another dir.
-    var url = 'http://localhost:63342/CT_2Nehpi_31/Learn_JSON_JS/sls.txt';
+    var url = 'http://localhost:63342/CT_2Nehpi_31/Learn_JSON_JS/sltl.json';
 
     $.ajax({
         type: "GET",
         crossOrigin: true,
         crossDomain: true,
-        data: "String",
+        data: "dddString",
         url: url,
         success: function (data) {
             globDat(data);
-            $("#id02").html(globals.sliced);
-            $("#id03").html(globals.all);
+            $("#id02").html(globals.sent01);
+            $("#id03").html(globals.sentLst);
             return data;
         }
     });
 
     function globDat(dat) {
-        globals['sliced'] = dat.slice(0, 50);
-        globals['all'] = "short slice from globDat(dat)> " + globals.sliced;
+        globals['sent01'] = dat[1];
+        globals['sentLst'] = dat;
         return dat
     }
-    function aa(){
-        globals['a'] = 123;
-        globals[123] = 'This is a hard coded value.';
-    }
     function w(){
-        console.log("globals.a >> " + globals.a);
-        console.log("globals.all >> " + globals.all);
-        console.log("globals.sliced >> " + globals.sliced);
-        //alert(globals.123); //will not work
-        console.log("globals.123 >> " + globals[123]); //that's OK.
+        console.log("globals.sent01 >> " + globals.sent01);
+        console.log("globals.sentLst >> " + globals.sentLst);
     }
-    aa();
-    //globDat(" and this is some text added in the globDat() invocation.");
 
     w();
 });
