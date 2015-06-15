90b7363c4a8bff1771e6a5a932ac25ae5beb7668
clif edwards <clif.edwards.2@gmail.com>
2015-06-15 12:14:44 -0400

    ver 2.4.1  using ChapterData date into Learn_JSON_JS.       TODO  can 't access pyCharm project  ChapterData directly from this Webstorm proj       I can get .json date from files made in python and manipulate then in the app.js file. !! Yea, finally.
    IN BRANCH:  LEARN_JSON

--- Learn_JSON_JS/app.js
+++ Learn_JSON_JS/app.js
@@ -3,35 +3,42 @@ $(document).ready(function() {
     //noinspection JSUndeclaredVariable
     globals = {
         "sentLst": "",
-        "sent01": "",
+        "sent02": "",
         "something_else": ""
     };
 
     // pull actual data from another dir.
-    var url = 'http://localhost:63342/CT_2Nehpi_31/Learn_JSON_JS/sltl.json';
+    var file_name = 'cd_sentlst_str.json';
+
+    var url = 'http://localhost:63342/CT_2Nehpi_31/Learn_JSON_JS/' + file_name;
+    // WANT to directly access fill in pyton project.
+    // TODO FIX so far this doesn't work
+    var url1 = 'http://localhost:63342//C://Users/CLIF//My Projects//.PyCharm30//SeeScriptures//dat//' + file_name;
 
     $.ajax({
         type: "GET",
         crossOrigin: true,
         crossDomain: true,
-        data: "dddString",
+        data: "String",
         url: url,
         success: function (data) {
             globDat(data);
-            $("#id02").html(globals.sent01);
-            $("#id03").html(globals.sentLst);
+            $("#id02").html(globals.sent02);
+            $("#id03").html(globals.sentLst[0]);
             return data;
         }
     });
 
     function globDat(dat) {
-        globals['sent01'] = dat[1];
+        globals['sent02'] = dat[2];
         globals['sentLst'] = dat;
+        console.log("globals.sent02 > " + globals['sent02']);
+        console.log("globals.sentLst.length > " + globals['sentLst'].length);
         return dat
     }
     function w(){
-        console.log("globals.sent01 >> " + globals.sent01);
-        console.log("globals.sentLst >> " + globals.sentLst);
+        console.log("globals.sent02 >>> " + globals['sent02']);
+        console.log("globals.sentLst.length >>> " + globals['sentLst'].length);
     }
 
     w();
