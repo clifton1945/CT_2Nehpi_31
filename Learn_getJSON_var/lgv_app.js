60be2acc061b4a098485e662816d6302590c5ae7
clif edwards <clif.edwards.2@gmail.com>
2015-06-16 14:01:18 -0400

    ver 2.6.0 begin using getJSON()  and USING it's returned data.
    IN BRANCH:  Learn_getJSON_var

---
+++ Learn_getJSON_var/lgv_app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    // 2015-06-16 12:59:46
+    // dir:  Learn_getJSON_var
+    // CAN I preserve the getJSON() return data??
+    //
+    // USE actual data.
+    //
+    var file_name;
+
+    file_name = 'cd_sentlst_str.json';
+    //
+    function tst(dat, that) {
+        // 'that' becomes .getJSON().this
+
+        var msg = 'IN getJSON():';
+        msg += 'dataType.' + that.dataType;
+        console.log(msg);
+    }
+
+    $.getJSON(file_name, function(jd) {
+        $('#id01').html(jd[8]);
+        $('#id02').html(jd[9]);
+        tst(jd, this);
+    });
+});
+
+
+
