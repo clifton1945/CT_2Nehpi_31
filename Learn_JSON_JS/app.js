b541d815588acb97e90a9c72e08be639d3cb353d
clif edwards <clif.edwards.2@gmail.com>
2015-06-16 10:55:26 -0400

    ver 2.5.1  STILL trying for a global approach to preserve the getJSON(data) . DO I ACTUALLY NEED TO????
    Using $.getJSON()  with actual json files from python.  In this case cd_sentlst_str.json.  Can manipulate the return in app.js.
    HAVE NOT gotten a way to save the object with in app.js, just .html( pointer to return valuse) them to some selector.
    IN BRANCH:  LEARN_JSON

--- Learn_JSON_JS/app.js
+++ Learn_JSON_JS/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // this un var-ed declaration makes it global.
     //noinspection JSUndeclaredVariable
-    globals = {
+    globals = {  // no var to keep global.
         "sentLst": "",
         "sent02": "",
         "something_else": ""
@@ -9,34 +9,21 @@ $(document).ready(function() {
 
     // pull actual data.
     var file_name = 'cd_sentlst_str.json';
-
     // pull actual data from another dir.
     var url = 'http://localhost:63342/CT_2Nehpi_31/Learn_JSON_JS/' + file_name;
     // WANT to directly access fill in python project.
     // TODO FIX so far this doesn't work
     var url1 = 'http://localhost:63342//C://Users/CLIF//My Projects//.PyCharm30//SeeScriptures//dat//' + file_name;
 
-    //$.ajax({
-    //    type: "GET",
-    //    crossOrigin: true,
-    //    crossDomain: true,
-    //    data: "String",
-    //    url: url,
-    //    success: function (data) {
-    //        globDat(data);
-    //        $("#id02").html(globals.sent02);
-    //        $("#id03").html(globals.sentLst);
-    //        //return data;
-    //    }
-    //});
-
-    $.getJSON(file_name, function(jd) {  // NOTE:   usl rather than file_name didn't work.
-        $('#id01').html(jd[6]);
+    $.getJSON(file_name, function(jd) {  // NOTE:   url rather than file_name didn't work.
+        $('#id01').html(jd[8]);
         $('#id02').html(jd[9]);
+        globals.sent02 = jd[2];
+        console.log("IN $.getJSON() hope to see globals.sent02 here. >" + globals.sent02);
     });
 
-    var xx = $('#id01').html();
-    console.log("I hope to see id01 here. >" + xx);
+    console.log("main() won't see $('#id01') here.>" + $('#id01').html());
+    // globals.sent02 empty when this executed.
 
     //function globDat(dat) {
     //    globals['sent02'] = dat[2];
@@ -50,7 +37,22 @@ $(document).ready(function() {
     //    console.log("globals.sent02 >>> " + globals['sent02']);
     //    console.log("globals.sentLst.length >>> " + globals['sentLst'].length);
     //}
-    //
     //w();
+    //$.ajax({
+    //    type: "GET",
+    //    crossOrigin: true,
+    //    crossDomain: true,
+    //    data: "String",
+    //    url: url,
+    //    success: function (data) {
+    //        globDat(data);
+    //        $("#id02").html(globals.sent02);
+    //        $("#id03").html(globals.sentLst);
+    //        //return data;
+    //    }
+    //});
+    console.log("last in  .ready>  hope to see globals.sent02 here. >" + globals.sent02);
 });
+
+
 
