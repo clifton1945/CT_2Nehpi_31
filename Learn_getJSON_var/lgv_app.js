b0242159567b64d9a0b8c2ffdad6fa9dc0aaa4fb
clif edwards <clif.edwards.2@gmail.com>
2015-06-16 15:48:54 -0400

    ver 2.6.2 refining and learning
    WIP using getJSON()  and HOW to get vars into both 'named inner anonymous functions' AND 'outer functions'.
    
    IN BRANCH:  Learn_getJSON_var

--- Learn_getJSON_var/lgv_app.js
+++ Learn_getJSON_var/lgv_app.js
@@ -1,31 +1,35 @@
+// 2015-06-16 12:59:46
+// dir:  Learn_getJSON_var
+// CAN I preserve the getJSON() return data??
+//
 $(document).ready(function() {
-    // 2015-06-16 12:59:46
-    // dir:  Learn_getJSON_var
-    // CAN I preserve the getJSON() return data??
-    //
     // USE actual data.
-    //
     var file_name;
-
     file_name = '../cd_sentlst_str.json';
 
+    // the way to get file.file data.
     $.getJSON(file_name, function(jd) {
         var log, that, msg;
+        msg = 'IN getJSON() {';
         that = this;  // accessible to inner functions
-        modify_page(jd);  // does the work on the page.
 
+        modify_page(jd, msg);  // does the work on the page.
+
+        // create an inner function for use of parent msg.
+        //    NOTE: msg is not passed in.
         log = function log() {
-            msg = 'IN getJSON():';
             msg += '\n   dataType: ' + that.dataType;
+            msg += '\n   url: ' + that.url;
             console.log(msg);
         }();
     });
 
-    function modify_page(dat) {
-    // use external function
-    // since most work will be done here
+    // use as an external function to apps.js
+    // since most work will be added here
+    function modify_page(dat, msg) {
         $('#id01').html(dat[8]);
         $('#id02').html(dat[9]);
+        msg += "  added in modify_page. ";
         console.log( 'sample of dat >>\n' + dat[0].slice(0, 30));
     }
     //
