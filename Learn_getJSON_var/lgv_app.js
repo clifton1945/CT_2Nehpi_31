7e64e8b3007c3945fa8a0715c8e6463cd347ce80
clif edwards <clif.edwards.2@gmail.com>
2015-06-16 15:30:39 -0400

    ver 2.6.1 begin using getJSON()  and HOW to get vars into both 'named inner anonymous functions' AND 'outer functions'.
    IN BRANCH:  Learn_getJSON_var

--- Learn_getJSON_var/lgv_app.js
+++ Learn_getJSON_var/lgv_app.js
@@ -7,20 +7,33 @@ $(document).ready(function() {
     //
     var file_name;
 
-    file_name = 'cd_sentlst_str.json';
-    //
-    function tst(dat, that) {
-        var msg = 'IN getJSON():';
-        msg += 'dataType.' + that.dataType;  // 'that' is .getJSON().this
-        msg += '#id01 now = ' + dat[8];  // dat is .getJSON.jd
-        console.log(msg);
-    }
+    file_name = '../cd_sentlst_str.json';
 
     $.getJSON(file_name, function(jd) {
-        $('#id01').html(jd[8]);
-        $('#id02').html(jd[9]);
-        tst(jd, this);
+        var log, that, msg;
+        that = this;  // accessible to inner functions
+        modify_page(jd);  // does the work on the page.
+
+        log = function log() {
+            msg = 'IN getJSON():';
+            msg += '\n   dataType: ' + that.dataType;
+            console.log(msg);
+        }();
     });
+
+    function modify_page(dat) {
+    // use external function
+    // since most work will be done here
+        $('#id01').html(dat[8]);
+        $('#id02').html(dat[9]);
+        console.log( 'sample of dat >>\n' + dat[0].slice(0, 30));
+    }
+    //
+    //function log(dat, that) {
+    //    var msg = 'IN getJSON():';
+    //    msg += '\n   dataType: ' + that.dataType;  // 'that' is .getJSON().this
+    //    msg += '\n   now #id01: \n' + dat[8];  // dat is .getJSON.jd
+    //}
 });
 
 
