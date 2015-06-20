b53ad929b0e692fbbac5609f3b7941c67556154f
clif edwards <clif.edwards.2@gmail.com>
2015-06-19 20:07:30 -0400

    ver 3.0.2  CLEANING UP and adding click to Header. STABLE IN BRANCH:master >> HTML_WIP

--- Learn_jQuery_2Nep31/LJ2_apps.js
+++ Learn_jQuery_2Nep31/LJ2_apps.js
@@ -7,21 +7,29 @@ $(document).ready(function() {
         return this.length !== 0;
     };
 
-    $("#stage").click(function () {
-        $("#stage").load('../2Nep31_1_Header.html');
-        console.log("Finished #stage.click in $(document).ready(function).");
-    });
+    var $header = $("#header"),
+        $hdiv = $("#hdiv"),
+        $verses = $("#verses"),
+        $vdiv = $("#vdiv");
 
-    $("#driver").click(function () {
-        var $md = $("#mydiv");
-        if ($md.contents().length > 0) {
-            $md.html("");
-            $("#driver").val("LOAD.");
-            console.log("UNLOADED #mydiv.");
+    $header.click(function () {
+        if ($hdiv.contents().length > 0) {
+            $hdiv.html("");
+            $header.val("LOAD Nep31_1_Header.html");
         } else {
-            $md.load('../2Nep31_1.html');
-            $("#driver").val("UNLOAD.");
-            console.log("LOADED #mydiv.");
+            $hdiv.load('../2Nep31_1_Header.html');
+            $header.val = "UNLOAD Nep31_1_Header.html";
+            console.log("Finished #header.click in $(document).ready(function).");
+        }
+    });
+    $verses.click(function () {
+        if ($vdiv.contents().length > 0) {
+            $vdiv.html("");
+            $verses.val("LOAD 2Nep31_1.html");
+        } else {
+            $vdiv.load('../2Nep31_1.html');
+            $verses.val("UNLOAD 2Nep31_1.html");
+            console.log("Finished #verses.click in $(document).ready(function).");
         }
     });
 
