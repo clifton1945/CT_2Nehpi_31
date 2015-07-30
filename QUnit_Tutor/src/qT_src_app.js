4cf8894f6e2e1a03e028a353fc4049316b5a6e56
clif edwards <clif.edwards.2@gmail.com>
2015-07-30 10:58:11 -0400

    SANDBOX JS TEXT STYLE .1

--- QUnit_Tutor/src/qT_src_app.js
+++ QUnit_Tutor/src/qT_src_app.js
@@ -2,7 +2,17 @@
  * Created by CLIF on 4/14/2015.
  */
 
-
+//UTILITIES
+function roundIt(num, dPt){
+    if(dPt === undefined) {
+        dPt = 1;
+    }
+    var f = Math.pow(10, dPt);
+    return parseFloat(Math.round(num * f ) / f )
+}
+function logIt( txt ) {
+    $(".console").text(txt)
+}
 function MyLog(text) {
     /**
      * used to see log immediately, rather than on next operation.
@@ -20,9 +30,10 @@ function countThisWord (targetArr) {
     var text = $('div').text();
     var result = text.replaceAll(/(bapt[^\s,-]+)/ig, "<span id='bap'>$&</span>");
 }
-
 function click_a_verse () {
-    $('p').click(function () {
+    $('.verses p').click(function () {
+        var $this = $(this);
+        logIt("in click_a_verse $(this)" + $this.length + ", " + $this.text().slice(0, 50));
         $(this).toggle(1000, function () {
             $(this).addClass('expand');
             $(this).toggle(1000);
@@ -32,9 +43,14 @@ function click_a_verse () {
         $('#1').load('header.html');
     })
 }
-
+function toggleVerse( that ) {
+    that.toggle(1000, function () {
+        that.addClass('expand');
+        that.toggle(1000);
+    });
+}
 var main = function () {
-    $(".verses").load('verses.html')
+    $(".verses").load('verses.html');
     click_a_verse();
     //findText();
     //countThisWord(['bapti', 'brother', "Jacob", "Holy Ghost"]);
--- QUnit_Tutor/tst/qT_tests.js
+++ QUnit_Tutor/tst/qT_tests.js
@@ -1,14 +1,14 @@
 
-QUnit.module("qT_index.html");
+QUnit.module("qT_index_tests.html");
     QUnit.test( "title.slice(0,5)", function( assert ) {
     var cut = $("title").text();
-    assert.equal( cut.slice(0, 5), "FILE:", " Expect first 5 characters of html file calling this ...tests.js" );
+    assert.equal( cut.slice(0, 5), "FILE:", " Expect first 5 characters of gT_indedx.html file calling this ...tests.js" );
 });
 
-QUnit.module("hello");
-    function saysHi(name) {
-        return "Hi, " + name;
-    }
-    QUnit.test('saysHi()', function( assert ) {
-        assert.equal(saysHi("Jack"), "Hi, Jack", "function outputs string correctly")
-    });
+QUnit.module("STYLE ");
+QUnit.test('I can see qT_src_app', function( assert ) {
+    assert.equal(roundIt(123.490, 0), 123, "")
+});
+QUnit.test('split click_a_verse to binder / function', function( assert ) {
+    assert.ok(false);
+});
