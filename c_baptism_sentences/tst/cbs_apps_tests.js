c4999bafa303dd762c6e4bb7a96bd70edbaf2bcc
clif edwards <clif.edwards.2@gmail.com>
2015-08-03 10:14:29 -0400

    v 7.9.1  MORE change to curNdx from ndxCur style.

--- c_baptism_sentences/tst/cbs_apps_tests.js
+++ c_baptism_sentences/tst/cbs_apps_tests.js
@@ -66,13 +66,13 @@ QUnit.test(" calc opacity/transparency", function (assert) {
     // The function( distance ndxTop:0 and ndxBot:F(length, delta)
     // Where verses near curNdx have hiOp; near ndxFar have loOp.
     // ndxFar >= 0 if in class:old - already read.
-    // ndxFar <= ndxMax if in class:new - haven't read it yet
-    var cut = function setOpacity(curNdx, ndxMax, ndxFar, hiOp, loOp) {
+    // ndxFar <= maxNdx if in class:new - haven't read it yet
+    var cut = function setOpacity(curNdx, maxNdx, ndxFar, hiOp, loOp) {
         // a small ndxDelta means current near top|bottom: i.e, near the extreme.
         var ndxDelta = curNdx - ndxFar
             , isCloser2Top = ndxDelta >= 0
             , opDelta = hiOp - loOp
-            , x = opDelta * ndxDelta / ndxMax
+            , x = opDelta * ndxDelta / maxNdx
             ;
         return x
     };
@@ -89,8 +89,8 @@ QUnit.module("objLiteral Structure", {
     beforeEach: function () {
         this.mv = {
             const: {
-                DLTA_CUR: 2,
-                NDX_MIN: 0
+                CUR_DLTA: 2,
+                MIN_NDX: 0
             },
             ndx: {
                 min: undefined,
@@ -105,31 +105,31 @@ QUnit.module("objLiteral Structure", {
                 new: undefined
             },
             // local globals
-            curNdx: 0,
-            verses: [],
+            //curNdx: 0,
+            //verses: [],
             config: function () {
                 this.ndx.cur = 0;  // default
                 this.validNdxOld = function() {
                     return this.ndx.min;  // always
                 };
-                this.validcurNdx = function(curNdx, ndxMax) {
+                this.validcurNdx = function(curNdx, maxNdx) {
                     curNdx = (curNdx >= 0 ) ? curNdx : 0;
-                    curNdx = (curNdx <= ndxMax ) ? curNdx : ndxMax;
+                    curNdx = (curNdx <= maxNdx ) ? curNdx : maxNdx;
                     return curNdx
                 };
-                this.validNdxNew = function (curNdx, dltaCur, ndxMax) {
-                    var ndxNew = curNdx + dltaCur
+                this.validNdxNew = function (curNdx, curDlta, maxNdx) {
+                    var ndxNew = curNdx + curDlta
                     // NOTE: e.g. 3 + dlta==2 will have cur:3,4; new:5
                         ;
-                    return ( ndxNew < ndxMax) ? ndxNew : ndxMax
+                    return ( ndxNew < maxNdx) ? ndxNew : maxNdx
                 };
-                this.validNdxMax = function( arrLength, dltaCur){
+                this.validmaxNdx = function( arrLength, curDlta){
                     // NOTE: an array.length is 1 more than actual length: 6.
                     // NOTE: BUT ndx + dltaCur:2 points one beyond ndx
                     // SO says pt to 6 - 1 as the last useable index
                     // THEN subtract just 1 more place to accomodate the dltaCur
                     // 6 - 1 - dltaCur + 1 === len - dlta
-                    return arrLength - dltaCur
+                    return arrLength - curDlta
                 };
                 // deltas
                 this.calcDltaOld = function () {
@@ -141,21 +141,21 @@ QUnit.module("objLiteral Structure", {
             },
             /**
              *  except for the two params, all other indexs ans deltas are communicated.
-             * @param ndxCurrent: the index of the current verse.
+             * @param currentNdx: the index of the current verse.
              * @param collection: the verses [<p> tags] of the chapter being read.
              */
-            init: function (ndxCurrent, collection) {
+            init: function (currentNdx, collection) {
                 // local globals EARLY. They are used in .config
                 this.verses = collection;
-                this.curNdx = ndxCurrent;
-                this.dlta.cur = this.const.DLTA_CUR;
+                this.curNdx = currentNdx;
+                this.dlta.cur = this.const.CUR_DLTA;
                 // config some definitions needed before following calcs.
-                this.ndx.min = this.const.NDX_MIN;
-                this.ndx.max = this.validNdxMax(collection.length, this.dlta.cur);
+                this.ndx.min = this.const.MIN_NDX;
+                this.ndx.max = this.validmaxNdx(collection.length, this.dlta.cur);
                 // now the rest
                 this.ndx.old = this.validNdxOld();
-                this.ndx.cur = this.validcurNdx(ndxCurrent, this.ndx.max);  // already assigned
-                this.ndx.new = this.validNdxNew(ndxCurrent, this.dlta.cur, this.ndx.max);
+                this.ndx.cur = this.validcurNdx(currentNdx, this.ndx.max);  // already assigned
+                this.ndx.new = this.validNdxNew(currentNdx, this.dlta.cur, this.ndx.max);
 
                 this.dlta.old = this.calcDltaOld();
                 this.dlta.new = this.calcDltaNew();
@@ -189,7 +189,7 @@ QUnit.test("init curNdx:1", function( assert ){
 });
 QUnit.test("init curNdx:2", function( assert ){
     var mv = this.mv;
-    assert.ok(mv.const.DLTA_CUR === 2);
+    assert.ok(mv.const.CUR_DLTA === 2);
     assert.equal(mv.ndx.cur, undefined, "EXP no ncdCur BEFORE config ");
     mv.config();
     mv.init(2, [0,1,2,3,4,5,6]);
