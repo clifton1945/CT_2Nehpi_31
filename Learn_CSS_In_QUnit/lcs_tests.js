1f3aa94162b21e7668beee5926db68bb4df6190a
clif edwards <clif.edwards.2@gmail.com>
2015-08-12 17:11:56 -0400

    v 8.0.6 STABLE   .wrapAll() and .unwrap()
      Learn_CSS_In_QUnit

--- Learn_CSS_In_QUnit/lcs_tests.js
+++ Learn_CSS_In_QUnit/lcs_tests.js
@@ -25,22 +25,42 @@ QUnit.test("compare lengths.", function (assert) {
     assert.equal($('p').toArray().length, 7);
 });
 QUnit.test("QUnit adds an extra <p>: length is 6 not 7 <p>s.", function (assert) {
-    assert.equal($('p').length, 7);
-    assert.equal($('p').toArray().length, 7);
-    var ary = $('p').toArray();
+    var $p = $("p");
+    assert.equal($p.length, 7);
+    assert.equal($p.toArray().length, 7);
+    var ary = $p.toArray();
     assert.equal(ary[5].outerHTML.slice(0,15), "<p id=\"6\">6 And"
     , "EXP the last <p>. ");
     assert.equal(ary[6].outerHTML.slice(0,15)
         , '<p id=\"qunit-te', "EXP: qunit adds another <p>");
 });
 QUnit.test("wrap pararaphs with a <div>", function (assert) {
-    // TODO: FIGURE OUT WHAT selector.add(x) adds EACH element of selector
-    // look out selector.wrap(x) wraps EACH element of selector
-    // look out selector.append(x) append EACH element of selector
-    var ary = $("p").toArray();
-    assert.expect(2);
-    assert.equal(ary[2].innerHTML.slice(0, 8), '3 For my');
-    assert.equal(ary[2].outerHTML.slice(0, 8), "<p id=\"3");
+    // try  selector.wrapAll(wrapping element) wraps the selector with wrapping element.
+    // look out selector.unwrap() unwraps EACH element's parent.
+    var $p = $("p"), $23 = $("p#2, p#3")
+        ;
+    $23.wrapAll("<div class='new'></div>");
+    assert.equal($p.length, 7, "EXP to see all of them.");
+    assert.equal($("div.new").length, 1, 'EXP one div.');
+    assert.equal($("div.new p").length, 2, 'EXP two <p> in the div.new');
+    $23.unwrap();
+    assert.equal($p.length, 7, "EXP to see all of them.");
+    assert.equal($("div.new").length, 0, 'EXP no div.new.');
+    assert.equal($("div.new p").length, 0, 'EXP no <p> in a div.new');
+    $("p#3, p#4, p#5, p#6").wrapAll("<div class='new'></div>");
+    assert.equal($p.length, 7, "EXP to see all of them.");
+    assert.equal($("div.new").length, 1, 'EXP one div.');
+    assert.equal($("div.new p").length, 4, 'EXP two <p> in the div.new');
+    $("p#3, p#4").unwrap();
+    $("p#3, p#4").wrapAll("<div class='cur'></div>");
+    $("p#5, p#6").wrapAll("<div class='new'></div>");
+    assert.equal($p.length, 7, "EXP to see all of them.");
+    assert.equal($("div.cur").length, 1, 'EXP 34 one div.cur');
+    assert.equal($("div.cur p").length, 2, 'EXP 34 two <p> in the div cur');
+    assert.equal($("div.new").length, 1, 'EXP 56 one div.new');
+    assert.equal($("div.new p").length, 2, 'EXP 56 two <p> in the div new');
+
+
 });
 
 
